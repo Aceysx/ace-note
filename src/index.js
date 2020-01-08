cd4e6b43c8e91cff76b633d5755616f26dde0d3b
aceysx <aceysx@163.com>
2020-01-08 20:52:30 +0800

    load pre store when startup ace note

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
-import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
+import {Provider} from 'react-redux'
+import {applyMiddleware, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import reducer from './reducers'
 import './css/overwrite-antd-style.css'
-const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+import updateStoreToLocalStorageMiddleware from "./components/middleware/update-store-to-local-storage-middleware";
+
+const initStore = () => {
+  let store = window.localStorage.getItem('store');
+  return store ? JSON.parse(store) : {}
+}
+
+const store = createStore(reducer, initStore(), applyMiddleware(thunkMiddleware, updateStoreToLocalStorageMiddleware))
 
 
 ReactDOM.render(
