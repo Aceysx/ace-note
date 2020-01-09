641721227ad48823b54b13538d1ca14fa0e129c6
aceysx <aceysx@163.com>
2020-01-09 20:07:06 +0800

    remove unused comment

--- src/components/middleware/update-store-to-local-storage-middleware.js
+++ src/components/middleware/update-store-to-local-storage-middleware.js
@@ -1,6 +1,6 @@
-const UpdateStoreToLocalStorageMiddleware = (store) => (next) => (action) => {
+const UpdateStoreToLocalStorageMiddleware = store => next => action => {
   window.localStorage.setItem('store',JSON.stringify(store.getState()))
-  return next(action);    // 执行下一步
+  return next(action)
 }
 
 export default UpdateStoreToLocalStorageMiddleware
