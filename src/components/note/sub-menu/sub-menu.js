234451befaa1bd30c8f19876ef74c018537ec98e
aceysx <aceysx@163.com>
2020-01-18 14:59:16 +0800

    order by name

--- src/components/note/sub-menu/sub-menu.js
+++ src/components/note/sub-menu/sub-menu.js
@@ -71,8 +71,8 @@ class SubMenu extends React.Component {
       else files.push(item)
     })
 
-    return [...dirs.sort((a, b) => a.name > b.name ? -1 : 1),
-      ...files.sort((a, b) => a.name > b.name ? -1 : 1)]
+    return [...dirs.sort((a, b) => a.name > b.name ? 1 : -1),
+      ...files.sort((a, b) => a.name > b.name ? 1 : -1)]
   }
 
   subFiles = selectedDir => {
