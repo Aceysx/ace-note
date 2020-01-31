d93e6d775f0586e7c1e43607de2a637edbec7fcf
aceysx <aceysx@163.com>
2020-01-31 17:01:16 +0800

    dev env starter

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const {app, BrowserWindow} = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
 require('./src/infrastructure/main-process/listener')
 let win
 
@@ -11,8 +13,13 @@ function createWindow() {
   })
   win.maximize()
   win.show()
-  win.loadURL('http://localhost:3000/')
-  win.webContents.openDevTools()
+  win.loadURL(isDev
+    ? 'http://localhost:3000/'
+    : `file://${path.join(__dirname, './build/index.html')}`)
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+
 }
 
 app.on('ready', createWindow)
