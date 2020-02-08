e9c62c7ab75b392a9f15f597a4f9faa04257311b
aceysx <aceysx@163.com>
2020-02-08 14:03:17 +0800

    :truck: move file logic code to note component from app

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,16 +1,13 @@
 import * as React from 'react'
-import {Empty, Layout, message} from 'antd'
+import {Empty, Layout} from 'antd'
 import {connect} from 'react-redux'
 import FileResource from '../infrastructure/file-resource'
 import Note from './note/note'
 import Setting from './setting/setting'
-import NoteTagModel from '../model/note-tag'
-import File from '../model/file'
 import GitResource from '../infrastructure/git-resource'
 import LeftMenu from './left-menu/left-menu'
 import {
   SELECTED_DIR_STACK,
-  UPDATE_CURRENT_EDIT_FILE,
   UPDATE_FILES,
   UPDATE_NOTES_TAGS,
   UPDATE_SELECTED_DIR
@@ -42,6 +39,44 @@ class App extends React.Component {
     this.props.updateNotesTags(FileResource.getNotesTags(window.getNoteTagsPath()))
   }
 
+  updateNotesTags = (path, notesTags) => {
+    FileResource.modifyFileContent({path, content: JSON.stringify(notesTags)})
+    this.props.updateNotesTags(FileResource.getNotesTags(window.getNoteTagsPath()))
+  }
+
+  isEmpty = (current, selectedDir) => {
+    return current === MENU.NOTE && selectedDir.sub === undefined
+  }
+
+  updateSelectedDir = (selectedDir, type) => {
+    if (type === 'search') {
+      this.props.updateSelectedDir(selectedDir)
+      return
+    }
+    if (selectedDir === MENU.SETTING) {
+      this.setState({current: MENU.SETTING})
+      return
+    }
+    this.setState({current: MENU.NOTE})
+    this.pushPathToSelectedDirStack(selectedDir);
+    this.props.updateSelectedDir(FileResource.findSubFiles(selectedDir))
+  }
+
+  pushPathToSelectedDirStack = path => {
+    if (path === '搜索结果') {
+      return
+    }
+    const {selectedDirStack} = this.props;
+    if (selectedDirStack[selectedDirStack.length - 1] !== path) {
+      selectedDirStack.push(path);
+      this.props.updateSelectedDirStack(selectedDirStack)
+    }
+  }
+
+  pushToRepo = (workspace) => {
+    GitResource.pushToRepo(workspace)
+  }
+
   resetWorkspace = () => {
     const workspace = this.initWorkspace()
     this.props.updateDirs(FileResource.initNoteBook(workspace))
@@ -56,140 +91,9 @@ class App extends React.Component {
     return workspace
   }
 
-  updateNotesTags = (path, notesTags) => {
-    FileResource.modifyFileContent({path, content: JSON.stringify(notesTags)})
-    this.props.updateNotesTags(FileResource.getNotesTags(window.getNoteTagsPath()))
-  }
-
-  pushPathToSelectedDirStack = path => {
-    if (path === '搜索结果') {
-      return
-    }
-    const {selectedDirStack} = this.props;
-    if (selectedDirStack[selectedDirStack.length - 1] !== path) {
-      selectedDirStack.push(path);
-      this.props.updateSelectedDirStack(selectedDirStack)
-    }
-  }
-
-  updateSelectedDir = path => {
-    if (path === MENU.SETTING) {
-      this.setState({current: MENU.SETTING})
-      return
-    }
-    this.setState({current: MENU.NOTE})
-    this.pushPathToSelectedDirStack(path);
-    this.props.updateSelectedDir(FileResource.findSubFiles(path))
-  }
-
-  modifyFileName = (oldPath, newFileName) => {
-    if (this._exist(newFileName)) {
-      message.warning('文件已存在')
-      return false;
-    }
-    if (this._validate(newFileName)) {
-      message.warning('文件名不能包含【\\\\/:*?\"<>|】')
-      return false;
-    }
-    const {selectedDir, currentEditFile} = this.props
-    let newPath = FileResource.modifyFileName({oldPath, newFileName})
-    if (currentEditFile.path === oldPath) {
-      this.props.updateCurrentEditFile(
-        FileResource.findFile(newPath)
-      )
-    }
-    this.updateSelectedDir(selectedDir.path)
-    this.props.updateDirs(FileResource.initNoteBook(window.getNoteWorkspacePath()))
-  }
-
-  modifyFileContent = (path, content) => {
-    this.props.updateCurrentEditFile(
-      FileResource.modifyFileContent({path, content})
-    )
-  }
-
-  createFileOrDir = ({path, type}) => {
-    let file = FileResource.createFileOrDir({type, path});
-    this.updateSelectedDir(path)
-    if (type === 'dir') {
-      this.props.updateDirs(FileResource.initNoteBook(window.getNoteWorkspacePath()))
-      return
-    }
-    this.props.updateCurrentEditFile(file)
-  }
-
-  deleteFileOrDir = ({path, type}) => {
-    const {selectedDir, notesTags} = this.props
-    FileResource.delete({path, type})
-    this.updateSelectedDir(selectedDir.path)
-
-    if (type === 'file') {
-      const _path = path.split(window.getNoteWorkspacePath())[1]
-      this.updateNotesTags(window.getNoteTagsPath(),
-        NoteTagModel.delete(_path, notesTags))
-      this.props.updateCurrentEditFile({})
-    }
-
-    if (type === 'dir') {
-      this.props.updateDirs(FileResource.initNoteBook(window.getNoteWorkspacePath()))
-    }
-  }
-  isEmpty = (current, selectedDir) => {
-    return current === MENU.NOTE && selectedDir.sub === undefined
-  }
-
-  pushToRepo = (workspace) => {
-    GitResource.pushToRepo(workspace)
-  }
-
-  searchFiles = content => {
-    const selectedDir = {
-      path: '搜索结果',
-      sub: this._searchByTitleOrTag(content)
-    }
-    this.props.updateSelectedDir(selectedDir)
-  }
-
-  _searchByTitleOrTag = content => {
-    const {leftMenu, notesTags} = this.props
-    const allFiles = this._formatAllFiles(leftMenu.sub)
-    const inFilesPath = allFiles.filter(file => {
-      return File.name(file.path).includes(content)
-    })
-    const foundInTags = notesTags.filter(tagFile => tagFile.tags.join(',').includes(content))
-    const inTagPath = allFiles.filter(file => foundInTags.find(tagFile => file.path.includes(tagFile.path)))
-    inFilesPath.push(...inTagPath)
-    return inFilesPath
-  }
-
-  _formatAllFiles = sub => {
-    const files = []
-    sub.forEach(item => {
-      if (item.type === 'dir') {
-        files.push(...this._formatAllFiles(item.sub))
-      }
-      if (item.type === 'file') {
-        files.push(item)
-      }
-    })
-    return files
-  }
-
-  _exist = fileName => {
-    return this.props.selectedDir.sub.filter(file => {
-      return File.name(file.path) === fileName
-    }).length === 1
-  }
-
-  _validate = fileName => {
-    const reg = new RegExp('[\\\\/:*?\"<>|]')
-    return reg.test(fileName)
-  }
-
   render() {
-    console.log(window.getNoteWorkspacePath())
     const {current} = this.state
-    const {leftMenu, selectedDir, currentEditFile, selectedDirStack, notesTags} = this.props
+    const {leftMenu, selectedDir, notesTags, selectedDirStack} = this.props
     return <Layout className='layout'>
       <Sider
         className='layout_left_sider'
@@ -206,19 +110,14 @@ class App extends React.Component {
           {
             current === MENU.NOTE && selectedDir.sub !== undefined
               ? <Note
-                createFileOrDir={this.createFileOrDir}
-                notesTags={notesTags}
-                selectedDir={selectedDir}
-                currentEditFile={currentEditFile}
-                updateCurrentEditFile={this.props.updateCurrentEditFile}
-                updateSelectedDir={this.updateSelectedDir}
-                deleteFileOrDir={this.deleteFileOrDir}
-                selectedDirStack={selectedDirStack}
-                updateSelectedDirStack={this.props.updateSelectedDirStack}
-                modifyFileContent={this.modifyFileContent}
-                modifyFileName={this.modifyFileName}
+                leftMenu={leftMenu}
                 updateNotesTags={this.updateNotesTags}
-                searchFiles={this.searchFiles}
+                notesTags={notesTags}
+                updateDirs={this.props.updateDirs}
+                updateSelectedDir={this.updateSelectedDir}
+                selectedDir={selectedDir}
+                updateSelectedDirStack={this.props.updateSelectedDirStack}
+                selectedDirStack={selectedDirStack}
               />
               : ''
           }
@@ -247,21 +146,18 @@ const mapDispatchToProps = dispatch => ({
   updateDirs: dirs => dispatch(UPDATE_FILES(dirs)),
   updateNotesTags: noteTags => dispatch(UPDATE_NOTES_TAGS(noteTags)),
   updateSelectedDir: dir => dispatch(UPDATE_SELECTED_DIR(dir)),
-  updateCurrentEditFile: file => dispatch(UPDATE_CURRENT_EDIT_FILE(file)),
   updateSelectedDirStack: selectedDirStack => dispatch(SELECTED_DIR_STACK(selectedDirStack)),
 })
 
 const mapStateToProps = ({
                            leftMenu,
                            selectedDir,
-                           currentEditFile,
                            selectedDirStack,
                            notesTags
                          }) => ({
   leftMenu,
   selectedDir,
   selectedDirStack,
-  currentEditFile,
   notesTags
 })
 export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/ui/note/note.js
+++ src/ui/note/note.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import {Empty} from 'antd'
+import {Empty, message} from 'antd'
 import SubMenu from './sub-menu/sub-menu'
 import Markdown from './markdown/markdown'
 import SearchBar from './search-bar'
+import {connect} from 'react-redux'
+import NoteTagModel from '../../model/note-tag'
+import File from '../../model/file'
+import FileResource from '../../infrastructure/file-resource'
+import {
+  UPDATE_CURRENT_EDIT_FILE,
+} from '../../redux/reducers/dispatch-command/commands'
 
-export default class Note extends React.Component {
+class Note extends React.Component {
   state = {
     isSubMenuFold: false
   }
@@ -16,11 +23,106 @@ export default class Note extends React.Component {
     this.setState({isSubMenuFold: false})
   }
 
+  modifyFileName = (oldPath, newFileName) => {
+    if (this._exist(newFileName)) {
+      message.warning('文件已存在')
+      return false;
+    }
+    if (this._validate(newFileName)) {
+      message.warning('文件名不能包含【\\\\/:*?\"<>|】')
+      return false;
+    }
+    const {selectedDir, currentEditFile} = this.props
+    let newPath = FileResource.modifyFileName({oldPath, newFileName})
+    if (currentEditFile.path === oldPath) {
+      this.props.updateCurrentEditFile(
+        FileResource.findFile(newPath)
+      )
+    }
+    this.props.updateSelectedDir(selectedDir.path)
+    this.props.updateDirs(FileResource.initNoteBook(window.getNoteWorkspacePath()))
+  }
+
+  modifyFileContent = (path, content) => {
+    this.props.updateCurrentEditFile(
+      FileResource.modifyFileContent({path, content})
+    )
+  }
+
+  createFileOrDir = ({path, type}) => {
+    let file = FileResource.createFileOrDir({type, path});
+    this.props.updateSelectedDir(path)
+    if (type === 'dir') {
+      this.props.updateDirs(FileResource.initNoteBook(window.getNoteWorkspacePath()))
+      return
+    }
+    this.props.updateCurrentEditFile(file)
+  }
+
+  deleteFileOrDir = ({path, type}) => {
+    const {selectedDir, notesTags} = this.props
+    FileResource.delete({path, type})
+    this.props.updateSelectedDir(selectedDir.path)
+
+    if (type === 'file') {
+      const _path = path.split(window.getNoteWorkspacePath())[1]
+      this.props.updateNotesTags(window.getNoteTagsPath(),
+        NoteTagModel.delete(_path, notesTags))
+      this.props.updateCurrentEditFile({})
+    }
+
+    if (type === 'dir') {
+      this.props.updateDirs(FileResource.initNoteBook(window.getNoteWorkspacePath()))
+    }
+  }
+
+  searchFiles = content => {
+    const selectedDir = {
+      path: '搜索结果',
+      sub: this._searchByTitleOrTag(content)
+    }
+    this.props.updateSelectedDir(selectedDir, 'search')
+  }
+
+  _searchByTitleOrTag = content => {
+    const {leftMenu, notesTags} = this.props
+    const allFiles = this._formatAllFiles(leftMenu.sub)
+    const inFilesPath = allFiles.filter(file => {
+      return File.name(file.path).includes(content)
+    })
+    const foundInTags = notesTags.filter(tagFile => tagFile.tags.join(',').includes(content))
+    const inTagPath = allFiles.filter(file => foundInTags.find(tagFile => file.path.includes(tagFile.path)))
+    inFilesPath.push(...inTagPath)
+    return inFilesPath
+  }
+
+  _formatAllFiles = sub => {
+    const files = []
+    sub.forEach(item => {
+      if (item.type === 'dir') {
+        files.push(...this._formatAllFiles(item.sub))
+      }
+      if (item.type === 'file') {
+        files.push(item)
+      }
+    })
+    return files
+  }
+
+  _exist = fileName => {
+    return this.props.selectedDir.sub.filter(file => {
+      return File.name(file.path) === fileName
+    }).length === 1
+  }
+
+  _validate = fileName => {
+    const reg = new RegExp('[\\\\/:*?\"<>|]')
+    return reg.test(fileName)
+  }
+
   render() {
     const {
-      selectedDir, currentEditFile, selectedDirStack, updateCurrentEditFile,
-      updateSelectedDir, modifyFileName, deleteFileOrDir,
-      updateSelectedDirStack, modifyFileContent,
+      selectedDir, currentEditFile, selectedDirStack,
       notesTags, updateNotesTags
     } = this.props
     const {isSubMenuFold} = this.state
@@ -30,17 +132,17 @@ export default class Note extends React.Component {
         isSubMenuFold
           ? ''
           : <SubMenu
-            notesTags={notesTags}
-            createFileOrDir={this.props.createFileOrDir}
-            updateNotesTags={updateNotesTags}
-            selectedDir={selectedDir}
+            modifyFileName={this.modifyFileName}
+            createFileOrDir={this.createFileOrDir}
+            deleteFileOrDir={this.deleteFileOrDir}
             currentEditFile={currentEditFile}
-            updateCurrentEditFile={updateCurrentEditFile}
-            updateSelectedDir={updateSelectedDir}
-            modifyFileName={modifyFileName}
-            deleteFileOrDir={deleteFileOrDir}
+            updateCurrentEditFile={this.props.updateCurrentEditFile}
+            updateNotesTags={this.props.updateNotesTags}
+            notesTags={notesTags}
+            updateSelectedDir={this.props.updateSelectedDir}
+            selectedDir={selectedDir}
+            updateSelectedDirStack={this.props.updateSelectedDirStack}
             selectedDirStack={selectedDirStack}
-            updateSelectedDirStack={updateSelectedDirStack}
           />
       }
       <div className={`layout_right_content_layout_right_content_markdown `}>
@@ -48,20 +150,29 @@ export default class Note extends React.Component {
           currentEditFile.path
             ? <Markdown file={currentEditFile}
                         notesTags={notesTags}
-                        modifyFileContent={modifyFileContent}
-                        modifyFileName={modifyFileName}
+                        modifyFileContent={this.modifyFileContent}
+                        modifyFileName={this.modifyFileName}
                         updateNotesTags={updateNotesTags}
                         isSubMenuFold={isSubMenuFold}
                         changeSubMenuFold={() => this.setState({isSubMenuFold: !isSubMenuFold})}
             />
-            : <div style={{margin:'50%'}}>
+            : <div style={{margin: '50%'}}>
               <Empty
                 description={false}/>
             </div>
         }
       </div>
       <SearchBar
-        searchFiles={this.props.searchFiles}/>
+        searchFiles={this.searchFiles}/>
     </div>
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  updateCurrentEditFile: file => dispatch(UPDATE_CURRENT_EDIT_FILE(file))
+})
+
+const mapStateToProps = ({currentEditFile}) => ({
+  currentEditFile
+})
+export default connect(mapStateToProps, mapDispatchToProps)(Note)
