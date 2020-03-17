7ddd6af6dc30239a880fae1b243ce1f9028a177b
aceysx <aceysx@163.com>
2020-03-17 20:24:12 +0800

    :sparkles: tag: sort tags

--- src/ui/tag/tag-display-box.js
+++ src/ui/tag/tag-display-box.js
@@ -18,38 +18,38 @@ class TagDisplayBox extends React.Component {
     const {colorEdit, tagValueEdit} = this.state
     const notesSet = new Set()
     Object.values(tagsNotes).forEach(tags => notesSet.add(...tags))
+    const tags = Object.keys(tagsNotes).sort()
 
     return <div><Statistic
       value={Object.keys(tagsNotes).length}
       suffix={`/ ${notesSet.size}`}/>
       {
-        Object.keys(tagsNotes)
-          .map((tag, index) => {
-            const [color, content] = NoteTagModel.format(tag)
-            return <Tag key={index}
-                        className='tag'
-                        color={color}>
-              <Popover content={<ColorPicker
-                color={colorEdit}
-                tagValue={tagValueEdit}
-                colorChange={colorEdit => this.setState({colorEdit})}
-                tagValueChange={tagValueEdit => this.setState({tagValueEdit})}
-                modify={() => this.modify(tag)}
-              />}>
+        tags.map((tag, index) => {
+          const [color, content] = NoteTagModel.format(tag)
+          return <Tag key={index}
+                      className='tag'
+                      color={color}>
+            <Popover content={<ColorPicker
+              color={colorEdit}
+              tagValue={tagValueEdit}
+              colorChange={colorEdit => this.setState({colorEdit})}
+              tagValueChange={tagValueEdit => this.setState({tagValueEdit})}
+              modify={() => this.modify(tag)}
+            />}>
                 <span
                   onMouseOver={() => {
                     this.setState({colorEdit: color, tagValueEdit: content})
                   }}
                   className='cursor_pointer'>🎨</span>
-              </Popover>
-              <span className='cursor_pointer'
-                    onClick={() => this.props.clickTag(tag)}>
+            </Popover>
+            <span className='cursor_pointer'
+                  onClick={() => this.props.clickTag(tag)}>
                 {content}
                 </span>
-              <Divider type={'vertical'}/>
-              <span style={{fontSize: '12px'}}> {tagsNotes[tag].length}</span>
-            </Tag>
-          })
+            <Divider type={'vertical'}/>
+            <span style={{fontSize: '12px'}}> {tagsNotes[tag].length}</span>
+          </Tag>
+        })
       }
     </div>
   }
