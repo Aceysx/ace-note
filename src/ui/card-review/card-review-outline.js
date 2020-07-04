9f7b7a727a607b4759531d4a1bbe8ff112da166b
aceysx <aceysx@163.com>
2020-07-04 23:50:32 +0800

    :sparkles: can render mindmap in card review outline

--- src/ui/card-review/card-review-outline.js
+++ src/ui/card-review/card-review-outline.js
@@ -1,71 +1,38 @@
 import React from "react";
-import {Divider, Empty, Icon, PageHeader} from "antd";
+import {Divider, Icon, PageHeader} from "antd"
 import File from '../../model/file'
-import CardReview from "../../model/card-review";
+import CardReview from "../../model/card-review"
+import MindMap from "../note/mindmap/mind-map"
 
-const TITLE = [
-  {layer: 1, start: '# '},
-  {layer: 2, start: '## '},
-  {layer: 3, start: '### '},
-  {layer: 4, start: '#### '},
-  {layer: 5, start: '##### '}
-]
+
 const CardReviewOutline = ({cardDetail, reviewCard, hideOutline}) => {
-  const convert = content => {
-    const result = []
-    content.split('\n').forEach((origin) => {
-      const found = TITLE.find(title => origin.startsWith(title.start))
-      if (found) {
-        const {layer, start} = found
-        result.push({
-          layer,
-          origin,
-          content: origin.replace(start, '')
+  return <PageHeader
+    style={{height: '100%'}}
+    title={File.name(cardDetail.path)}
+    extra={[<a style={{color: '#b7906b', fontStyle: 'italic'}}
+               onClick={hideOutline}>start review <Icon type="arrow-right"/></a>]}
+    subTitle={<div>
+      <Divider type='vertical'/>
+      <span style={{fontStyle: "italic", fontSize: 12}}>  history review </span>
+      {reviewCard.history.length
+        ? reviewCard.history.map(his => {
+          return CardReview.getStatusIcon(his.status)
         })
+        : CardReview.getStatusIcon(CardReview.STATUS.NOT_REVIEW)
       }
-    })
-    return result
-  }
-
-  const tree = convert(cardDetail.content)
-  return <div>
-    <PageHeader
-      title={File.name(cardDetail.path)}
-      extra={[<a style={{color: '#b7906b', fontStyle: 'italic'}}
-                 onClick={hideOutline}>start review <Icon type="arrow-right"/></a>]}
-      subTitle={<div>
-        <Divider type='vertical'/>
-        <span style={{fontStyle: "italic", fontSize: 12}}>  history review </span>
-        {reviewCard.history.length
-          ? reviewCard.history.map(his => {
-            return CardReview.getStatusIcon(his.status)
-          })
-          : CardReview.getStatusIcon(CardReview.STATUS.NOT_REVIEW)
-        }
-      </div>}
-    >
-    <span style={{
-      fontSize: 14,
-      display: 'block',
-      marginBottom: 5,
-      fontWeight: 'bold',
-      fontStyle: 'italic',
-      color: 'rgba(25, 23, 17, 0.8)'
-    }}>outline</span>
-      <Divider/><br/>
-      {tree.length
-        ?
-        tree.map((item, index) => {
-          return <span key={index} className='review-outline-item cursor_pointer'
-                       style={{
-                         paddingLeft: `${10 * (item.layer - 1)}px`,
-                         opacity: `${(1 - (2 + item.layer) / 10) < 0.5 ? 0.5 : (1 - (2 + item.layer) / 10)}`
-                       }}>{item.content.trim()}</span>
-        })
-        : <Empty/>
-      }
-    </PageHeader>
-  </div>
+    </div>}
+  >
+      <span style={{
+        fontSize: 14,
+        display: 'block',
+        marginBottom: 5,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        color: 'rgba(25, 23, 17, 0.8)'
+      }}>outline</span>
+    <Divider/><br/>
+    <MindMap markdown={cardDetail.content}/>
+  </PageHeader>
 }
 
 export default CardReviewOutline
---
+++ src/ui/note/mindmap/mind-map.js
@@ -0,0 +1,63 @@
+import React from "react"
+import {loadPlugins, Markmap} from 'markmap-lib/dist/view'
+import {transform} from 'markmap-lib/dist/transform'
+import Empty from "antd/es/empty";
+
+export default class MindMap extends React.Component {
+  componentDidMount() {
+    const {markdown} = this.props
+    if (this.isRenderMindMap(markdown)) {
+      this.renderMindMap(markdown);
+    }
+  }
+
+  isRenderMindMap = (markdown= '') => {
+    return markdown.split('\n').some(line => {
+      let trimLine = line.trimLeft();
+      return trimLine.startsWith('#')
+    })
+  }
+
+  format = markdown => {
+    return markdown.split('\n').filter(line => {
+      let trimLine = line.trimLeft();
+      trimLine = trimLine.substr(0, 4)
+      return trimLine.startsWith('#')
+        || trimLine.startsWith('##')
+        || trimLine.startsWith('###')
+        || trimLine.startsWith('####')
+    }).join('\n')
+  }
+
+  renderMindMap = markdown => {
+    const data = transform(this.format(markdown))
+    let e = document.querySelector("#markmap");
+    e.innerHTML = "";
+
+    loadPlugins([
+      'mathJax',
+      'prism',
+    ]).then(() => {
+      Markmap.create('#markmap', null, data)
+    })
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    const {markdown} = nextProps
+    if (this.props.markdown !== markdown && this.isRenderMindMap(markdown)) {
+
+      this.renderMindMap(markdown)
+    }
+  }
+
+  render() {
+    const {markdown} = this.props
+    return <div>
+      {
+        this.isRenderMindMap(markdown)
+          ? <svg id="markmap" width={'100%'} height={700}/>
+          : <Empty/>
+      }
+    </div>
+  }
+}
