4f317c90cb534d3a9c00aea208fe3c3e2ae7ce7b
aceysx <aceysx@163.com>
2020-09-02 13:14:27 +0800

    :ok_hand: update timeCard template

--- src/ui/timecard/template/plan-template.js
+++ src/ui/timecard/template/plan-template.js
@@ -3,16 +3,15 @@ export default [
     title: "周内",
     content: `
     [
-      {"label":{"1":1},"title":"早起运动"},
       {"label":{"4":1},"title":""},
       {"label":{"12":24},"title":"工作"},
-      {"label":{"11":4},"title":"中午"},
+      {"label":{"10":4},"title":"中午"},
       
-      {"label":{"11":4},"title":""},
+      {"label":{"10":4},"title":""},
       
       {"label":{"2":3},"title":"娱乐"},
       {"label":{"7":1},"title":"制定计划&总结"},
-      {"label":{"6":19},"title":"睡觉"}
+      {"label":{"6":21},"title":"睡觉"}
     ]
     `
   }, {
@@ -22,11 +21,11 @@ export default [
       {"label":{"1":1},"title":"早起运动"},
       {"label":{"4":1},"title":""},
       
-      {"label":{"11":4},"title":""},
+      {"label":{"10":4},"title":""},
       
       {"label":{"2":3},"title":"娱乐"},
       {"label":{"7":1},"title":"制定计划&总结"},
-      {"label":{"6":19},"title":"睡觉"}
+      {"label":{"6":21},"title":"睡觉"}
     ]
     `
   }
