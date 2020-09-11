bbb911cff75549cef3a596d3377fde8a497461ec
aceysx <aceysx@163.com>
2020-09-11 13:14:57 +0800

    :sparkles: add menu tabs of timecard

--- src/ui/timecard/timecard-body.js
+++ src/ui/timecard/timecard-body.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Divider, Icon, Modal} from "antd"
+import {Divider, Icon, Modal, Tabs} from "antd"
 
 import TitleBar from "../title-bar/title-bar"
 import TimecardCalendar from "./timecard-calendar"
@@ -10,8 +10,12 @@ import TimecardModel from "../../model/timecard"
 import {publish} from "../event/publish-event"
 import {UPDATE_TIMECARD_LABELS, UPDATE_TIMECARD_PLANS} from "../../redux/reducers/dispatch-command/commands"
 import {TIMECARD_PLAN_STATUS_CHANGE} from "../event/event"
-
+import PLAN_ICON from '../../resources/images/plan.png'
+import STATISTICS_ICON from '../../resources/images/statistics.png'
+import SETTINGS_ICON from '../../resources/images/settings.png'
 import '../../resources/css/timecard.css'
+
+const {TabPane} = Tabs
 
 class TimecardBody extends React.Component {
   state = {
@@ -62,18 +66,47 @@ class TimecardBody extends React.Component {
         leftMenuVisible={leftMenuVisible}
         changeLeftMenuVisible={this.props.updateStatus}
         pushToRepo={this.props.pushToRepo}/>
-      <div style={{height: 35}}/>
-      <TimecardCalendar
-        plans={this.filterDailyPlans(timecardPlans)}/>
-      <Divider/>
+      <Tabs defaultActiveKey="1"
+            style={{textAlign: 'center', zIndex: 1000}}>
+        <TabPane tab={
+          <span className='cursor_pointer'>
+            <img src={PLAN_ICON} width={18}/>
+            <Divider type={'vertical'}/>
+            Plans
+          </span>} key="1">
+          <div style={{textAlign: 'left'}}>
+            <TimecardCalendar
+              plans={this.filterDailyPlans(timecardPlans)}/>
+            <Divider/>
+            <TimecardPlansBody
+              labels={timecardLabels}
+              plans={timecardPlans}
+              edit={this.editPlan}
+              del={this.delPlan}
+              create={plan => TimecardModel.createPlan(plan)}
+            />
+          </div>
+        </TabPane>
+        <TabPane tab={
+          <span className='cursor_pointer'>
+            <img src={STATISTICS_ICON} width={18}/>
+            <Divider type={'vertical'}/>
+            Statistic
+          </span>
+        } key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab={
+          <span className='cursor_pointer'>
+            <img src={SETTINGS_ICON} width={18}/>
+            <Divider type={'vertical'}/>
+            Settings
+          </span>
+        } key="3">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>
 
-      <TimecardPlansBody
-        labels={timecardLabels}
-        plans={timecardPlans}
-        edit={this.editPlan}
-        del={this.delPlan}
-        create={plan => TimecardModel.createPlan(plan)}
-      />
 
       <Modal
         title="Create plan"
